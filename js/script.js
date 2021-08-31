96e6d759e39a075b6c10b9d8311f7a75b69d67d6
tonMoraes <tondevspace@gmail.com>
2021-08-31 09:56:35 -0300

    fixing iterators errors on js file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
+//'json' part
+
+let api = [
+  {
+    "id": "01",
+    "type": "Roupa feminina",
+    "productName": "Jaqueta Feminina",
+    "productColor": "Preta",
+    "value": "R$ 450,00",
+    "productSizes": ["XP", "P", "M", "G", "XG"],
+    "details": "Jaqueta feminina em material sintético na cor preta.",
+    "fit": "O tamanho XP veste x, o tamanho P veste p..."
+  },
+  {
+    "id": "02",
+    "type": "Roupa feminina",
+    "productName": "Jaqueta Feminina",
+    "productColor": "Sarge",
+    "value": "R$ 450,00",
+    "productSizes": ["XP", "P", "M", "G", "XG"],
+    "details": "Jaqueta feminina em material sintético na cor preta.",
+    "fit": "O tamanho XP veste x, o tamanho P veste p..."
+  }
+]
+
+// Interactive part of the code;
+
 let thumbs = document.querySelectorAll('.ico');
 const mainImg = document.querySelector('.main-img');
 
-for(let t = 0; t <= thumbs.length; t++) {
+for(let t = 0; t < thumbs.length; t++) {
+
+  thumbs[0];
   thumbs[t].addEventListener('mouseover', () => {
     function setImg() {
       mainImg.setAttribute('src', `./assets/img${t}.jpg`);
@@ -26,10 +55,12 @@ for(let t = 0; t <= thumbs.length; t++) {
   });
 }
 
+// Modeling class to create an render an element on page
 
 class Product {
   constructor (id, type, productName, productColor, value, productSizes, details, fit) {
     this.id = id;
+    this.type = type;
     this.productName = productName;
     this.productColor = productColor;
     this.value = value;
@@ -37,4 +68,25 @@ class Product {
     this.details = details;
     this.fit = fit;
   }
+}
+
+let result = {...api[0]};
+const p = document.querySelector('#root');
+
+const jaqueta = new Product(result.id, result.type, result.productName, result.productColor, result.value, result.productSizes, result.details, result.fit);
+
+let imprimeProduto = function (produto) {
+  return `
+  <p><strong>Meu Produto</strong><br>
+    ${produto.productName}<p>
+  <p><strong>${produto.value}</strong><p>
+  <p>Cor: ${produto.productColor}<p>
+
+  <p>Tamanhos: ${produto.productSizes}<p>
+
+  <p>Detalhes: ${produto.details}<p>
+
+  <p>Medidas: ${produto.fit}<p>
+  
+  `
 }
